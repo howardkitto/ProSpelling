e6b6c8cf76f7aee5f78c8807669c9970d2c7aac0
Teh126 <yvettekitto@gmail.com>
2017-10-11 23:11:19 +0100

    letter sound background colour added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import './css/App.css'
 import a from './audio/a.mp3'
 import e from './audio/e.mp3'
 import i from './audio/i.mp3'
+import o from './audio/o.mp3'
+import u from './audio/u.mp3'
 import hand from './images/righthandoutline.png'
 
 import Letter from './letter'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+
 
   playSound(theSrc){
     this.audioPlayer.src = theSrc
@@ -32,11 +32,13 @@ class App extends Component {
       <div className="container">
         <audio type="audio/mpeg"
         ref={(audio) => { this.audioPlayer = audio; }}/>
-        <div className="jumbotron">
-          {this.renderLetter('a', a)}
-          {this.renderLetter('e', e)}
-          {this.renderLetter('i', i)}
-        </div>
+        
+         <div className="a_box"> {this.renderLetter('a', a)}</div>
+         <div className="e_box"> {this.renderLetter('e', e)}</div>      
+         <div className="i_box"> {this.renderLetter('i', i)}</div>  
+         <div className="o_box"> {this.renderLetter('o', o)}</div>  
+         <div className="u_box"> {this.renderLetter('u', u)}</div>  
+        
 <img src={hand} alt='hand'/>
 
       </div>
