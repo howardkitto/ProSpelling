0a7ffd6e8a67324635350b57a152cd1ae40f1899
Howard Kitto <kitto.howard@gmail.com>
2017-10-12 20:37:28 +0100

    server works

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 
 console.log('foo')
 
-
+app.use(express.static('client/build'));
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running on http://localhost:8080 or http://127.0.0.1:8080 ');
