cea58b775c1833a93b27e3d7de813789697da08d
Teh126 <yvettekitto@gmail.com>
2017-10-13 09:29:11 +0100

    home page changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import HomeContainer from './HomeContainer'
 import LessonOneContainer from './LessonOneContainer'
+import LessonTwoContainer from './LessonTwoContainer'
 
 import {
     BrowserRouter as Router,
@@ -18,12 +19,17 @@ class App extends Component {
       
       <Router>
       <div className="container">
-        
-        <Link to="/">Home</Link>
-        <Link to="/lessonone">Lesson One</Link>
-        
+    
+      <nav className="navbar navbar-dark bg-dark">
+            <Link className="nav-item nav-link active"to="/">Home</Link>
+            <Link className="nav-item nav-link" to="/lessonone">Lesson One</Link>
+            <Link className="nav-item nav-link" to="/lessontwo">Lesson Two</Link>
+        </nav>
+
+
         <Route exact path="/" component={HomeContainer}/>
         <Route path="/lessonone" component={LessonOneContainer}/>
+        <Route path="/lessontwo" component={LessonTwoContainer}/>
         </div>  
     </Router>
     
--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -7,7 +7,25 @@ render(){
 
     return(
 
-        <h1>Home</h1>   
+        <div className="container">
+           <div className="jumbotron">
+           <h1 className="display-3">Hello, world!</h1>
+           <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+           </div>
+  <div className="row">
+    <div className="col">
+      1 of 3
+    </div>
+    <div className="col">
+      2 of 3
+    </div>
+    <div className="col">
+      3 of 3
+    </div>
+    </div>
+    </div>
+    
+      
 
         )
     }
