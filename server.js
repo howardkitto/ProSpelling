37538006311223e31c435c2a1e0491f9a6f98941
Howard Kitto <kitto.howard@gmail.com>
2017-10-13 05:49:51 +0100

    test server running

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
 
-console.log('foo')
+// console.log('foo')
 
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World! ')
+})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running on http://localhost:8080 or http://127.0.0.1:8080 ');
