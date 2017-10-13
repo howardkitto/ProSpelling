56ba21fa4168ee1bd19ad8043af3ccaeba895684
Howard Kitto <kitto.howard@gmail.com>
2017-10-13 21:10:36 +0100

    compressed video

--- frontend/src/HomeContainer.js
+++ frontend/src/HomeContainer.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import rosie from './audio/Rosie.mp4'
+import rosie from './audio/Rosiecomp.mp4'
 
 
 class HomeContainer extends Component{
+
+
+
+
+updateCanvas(){
+  
+}  
+
+componentDidMount(){
+  let context = this.videoCanvas.getContext("2d")  
+  requestAnimationFrame(this.updateCanvas);
+
+}  
 
 render(){
 
@@ -13,9 +26,12 @@ render(){
            <h1 className="display-3">Hello, world!</h1>
            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
            </div>
-    <div className="col">
-      <video src={rosie} type="video/mp4" className="videoCanvas" autoPlay loop muted/>
-</div>
+
+      <video src={rosie} type="video/mp4" className="video" autoPlay loop muted
+              ref={(video) => { this.sourceVideo = video; }} />
+
+      <canvas   className="videoCanvas"
+                ref={(canvas) => { this.videoCanvas = canvas; }} />
   </div>
     
       
