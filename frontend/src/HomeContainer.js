f93c24c57aad37248cd22af929a3744f3db6ee30
Howard Kitto <kitto.howard@gmail.com>
2017-10-14 11:33:58 +0100

    ready to deploy with video

--- frontend/src/HomeContainer.js
+++ frontend/src/HomeContainer.js
@@ -26,7 +26,7 @@ render(){
         <div className="container">
            <div className="jumbotron">
            <h1 className="display-3">Hello, world!</h1>
-           <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+           <p className="lead">Video Version</p>
            </div>
 
       <video  src={rosie} 
