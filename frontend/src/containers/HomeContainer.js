2e8c3a1d693f684b0d6ff6ab19a154256e70b06f
Teh126 <yvettekitto@gmail.com>
2017-10-20 22:27:12 +0100

    changes

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -14,14 +14,14 @@ render(){
   return(
 
     <div className="container">
-      {(this.props.spellerName)?
-        <h1 className="display-3">Hello, {this.props.spellerName}</h1>:
-        <UserDetailsForm headerText = "What is your name?  "/>}
+      
         <Video  src={rosie}
               height = {180}
               width = {302}
               muted = {true}/>
-      
+              {(this.props.spellerName)?
+        <h1 className="display-3">Hello, {this.props.spellerName}</h1>:
+        <UserDetailsForm headerText = "What is your name?  "/>}
   </div>
         )
     }
