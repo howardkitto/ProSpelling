43e09ce8f3e837bb67b2cabe27ef5a868dddd995
Howard Kitto <kitto.howard@gmail.com>
2017-10-20 12:37:52 +0100

    deploy server test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # prospelling
 
 Howard and Yvette are an awesome team!
+
+Test Prod like this...
+
+cd frontend && npm run build && cd .. && NODE_ENV=production node server
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//cd frontend && npm run build && cd .. && NODE_ENV=production node server
+
 const app = require('./server/app.js')
+
 
 app.app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running on http://localhost:8080 or http://127.0.0.1:8080 ');
