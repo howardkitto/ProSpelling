8e8dc77b4fd4a01b91a03f7ee703bfa34fa3caf8
Howard Kitto <kitto.howard@gmail.com>
2017-10-26 15:26:12 +0100

    fixed lesson one audio

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,15 +1,17 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import speller from './reducers/speller'
 import createSagaMiddleware from 'redux-saga'
 import saga from './saga';
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const defaultState={}
 
 //TO DO: combine reducers into a root reducer
 const  store = createStore( speller,
                             defaultState,
-                            applyMiddleware(sagaMiddleware))
+                            composeEnhancers(applyMiddleware(sagaMiddleware))
+)
                             
 
 sagaMiddleware.run(saga);
