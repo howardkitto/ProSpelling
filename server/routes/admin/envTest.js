55823a3409379c4c74446b2747b7a600365cc068
Howard Kitto <kitto.howard@gmail.com>
2017-10-28 08:11:55 +0100

    hello test

--- server/routes/admin/envTest.js
+++ server/routes/admin/envTest.js
@@ -7,21 +7,23 @@ var env = {}
 router.route('/')
 .get((req, res)=>{
 
-    var promise = Words.count().exec()
+    // var promise = Words.count().exec()
 
-    promise.then((count)=>{
-        env.message = 'Hello World'
-        env.NODE_ENV = process.env.NODE_ENV
-        env.MONGO_HOST = process.env.MONGO_HOST;
-        env.wordCount = count;
-    })
-    .then(()=>{
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(env)) 
-    })
-    .catch((err)=>{
-        console.log('error ' + err)
-        })
+    // promise.then((count)=>{
+    //     env.message = 'Hello World'
+    //     env.NODE_ENV = process.env.NODE_ENV
+    //     env.MONGO_HOST = process.env.MONGO_HOST;
+    //     env.wordCount = count;
+    // })
+    // .then(()=>{
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.send(JSON.stringify(env)) 
+    // })
+    // .catch((err)=>{
+    //     console.log('error ' + err)
+    //     })
+
+    res.json({hello:"world"})
 })
 
 module.exports = router
