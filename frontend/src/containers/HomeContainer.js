d7904250b68563876fb9d6902beefa59fca55167
Howard Kitto <kitto.howard@gmail.com>
2017-11-22 16:17:15 +0000

    background video working

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -4,36 +4,7 @@ import { withRouter } from 'react-router-dom'
 
 import {Button} from 'reactstrap'
 
-import Video from './Video'
-
 class HomeContainer extends Component{
-
-  constructor(){
-    super()
-    this.state={
-      clickToPlay:false
-    }
-  }
-
-
-checkAutoplay()
-{
-  var promise = document.querySelector('video').play();
-  
-  if (promise !== undefined) {
-      promise.catch(error => {
-        console.log('false')
-          return false
-      }).then(() => {
-        console.log('true')
-          return true
-      });
-  }
-}
-
-componentDidMount(){
-  this.setState({clickToPlay:this.checkAutoplay()})
-}
 
 render(){
 
@@ -48,11 +19,17 @@ render(){
 
   return(
     <div>
-      {(this.state.showStartScreen)?
-      <div>Show start button</div>:
-      <div><Video src= 'https://s3-us-west-2.amazonaws.com/prospelling/video/test4.mp4'
-            showPlayButton={this.state.clickToPlay}/></div>}
-    </div>      
+      <video autoPlay muted loop playsInline id="myVideo">
+        <source src= 'https://s3-us-west-2.amazonaws.com/prospelling/video/test4.mp4' type="video/mp4"/>
+      Browser doesn't support html5 video
+      </video>
+      <div id="homepageContent">
+       <h1>Welcome to Pro Spelling</h1>
+        <div>
+        <LetsStartButton />
+        </div>
+      </div>
+      </div>
         )
     }
 }
@@ -69,17 +46,3 @@ export default connect(mapStateToProps)(HomeContainer)
 //<div className="stateDisplay">  <div>spellingTest: {this.props.spellingTestState}</div>
 //                                                <div>question: {this.props.questionState}</div>
 //                </div>
-
-
-
-   {/* 
-        <div>Click here to start</div>:
-        <div>
-          <div className="letsStart">
-             <div className="letsStartText">
-                <h1>Welcome to Pro Spelling</h1>
-             </div>
-             <LetsStartButton />
-           </div>
-           
-         </div>} */}
